8a8fee797b43f02bde40defebb5d0f10ac950e27
Natraj16 <154439734+Natraj16@users.noreply.github.com>
2025-04-26 10:52:00 +0000

    variable_dec

---
+++ basics/01_variable.js
@@ -0,0 +1,21 @@
+const account_Id=455633
+let accountEmail="natarajkashyap1@gmail.com"
+var accountPass="123456"
+accountCity="Jaipur"
+let accountState;
+
+
+//  account_Id=2 //Not allowed
+
+accountEmail="natkashyap1@gmail.com"
+accountPass="15963"
+accountCity="Patna"
+
+
+/*
+Prefer not to use var
+because of issue in block scope and functional scope
+
+*/
+console.log(account_Id);
+console.table([accountEmail,accountPass,account_Id,accountCity])
