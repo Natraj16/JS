c16abb06552335c38f73232f38ca5dd0a821e754
Natraj16 <154439734+Natraj16@users.noreply.github.com>
2025-04-26 11:44:12 +0000

    data type

---
+++ basics/02_datatype.js
@@ -0,0 +1,28 @@
+
+"use strict";//treat all JS code as newer version
+
+// alert("hello")
+// alert(3+3) we are using nodejs, not browser
+
+console.log(3+
+    3);  //code readablity should be high
+console.log("Raj");
+
+let name="Raj"
+let age=19
+let isLoogedIn=true
+
+//int ->2 to the powered 53
+//bigint
+//string->" " or ' '
+//null -> standalone value
+//undefined -> when no value is assigned to the variable
+//symbol -> helps in the case where uniqueness is required
+ 
+//above are the primitive data type
+
+// object is also data type
+
+console.log(typeof null);
+console.log(typeof undefined);
+
