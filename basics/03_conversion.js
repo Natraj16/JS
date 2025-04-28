c3735b6f21de1c949264246825291d2008dff32b
Natraj16 <154439734+Natraj16@users.noreply.github.com>
2025-04-28 17:30:36 +0000

    conversion

--- basics/03_conversion.js
+++ basics/03_conversion.js
@@ -1,47 +1,88 @@
-let score="33"
-console.log(typeof score);
-console.log(typeof(score));
+// let score="33"
+// console.log(typeof score);
+// console.log(typeof(score));
 
-let valueInNumber=Number(score)
-console.log(typeof valueInNumber);
+// let valueInNumber=Number(score)
+// console.log(typeof valueInNumber);
 
-let b="33lkj";
-let c=Number(b);
-console.log(c);
+// let b="33lkj";
+// let c=Number(b);
+// console.log(c);
 
-let d=null
-let e=Number(d)
-console.log(e);
+// let d=null
+// let e=Number(d)
+// console.log(e);
 
-let f=undefined;
-let g=Number(f)
-console.log(g);
+// let f=undefined;
+// let g=Number(f)
+// console.log(g);
 
-let z=true
-let y=Number(z)
+// let z=true
+// let y=Number(z)
+// console.log(y);
+
+// //"33"->33
+// // "33abc"->nan
+// //true -> 1
+
+// let isloggedIn=1
+// let u=Boolean(isloggedIn)
+// console.log(u)
+
+// // 1-> true 0 ->false
+
+// let i=""
+// let k="Raj";
+// let j=Boolean(i)
+// let p=Boolean(k);
+// console.log(j);
+// console.log(p);;
+
+// //""(empty string)->false
+// //"Raj"(quotes with value) -> true
+
+// let num=33
+// let stringnum=String(num)
+// console.log(stringnum);
+// console.log(typeof stringnum);
+
+
+// ****************Operations*****************
+
+// let value=3
+// let negvalue=-value;
+// console.log(negvalue);
+
+// console.log(2+2);
+// console.log(2*2);
+// console.log(2-2)
+// console.log(2/2);
+// console.log(2**2);
+// console.log(2%2);
+
+// let str1="hello world"
+// let str2=" goodmorning"
+
+// let str3=str1+str2
+// console.log(str3);
+
+
+// console.log("1"+2)
+// console.log(1+"2");
+// console.log("1"+2+2);
+// console.log(1+2+"2")
+
+// console.log((3+4)*5%3)
+
+// console.log(+true); //converted into number(tricky conversion)
+// // console.log(true+); //not converted
+
+// console.log(+"");
+
+// let num1, num2, num3
+
+// num1= num2= num3 =2+2 
+
+let x=3
+const y=++x
 console.log(y);
-
-//"33"->33
-// "33abc"->nan
-//true -> 1
-
-let isloggedIn=1
-let u=Boolean(isloggedIn)
-console.log(u)
-
-// 1-> true 0 ->false
-
-let i=""
-let k="Raj";
-let j=Boolean(i)
-let p=Boolean(k);
-console.log(j);
-console.log(p);;
-
-//""(empty string)->false
-//"Raj"(quotes with value) -> true
-
-let num=33
-let stringnum=String(num)
-console.log(stringnum);
-console.log(typeof stringnum);
