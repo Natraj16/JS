c5a10aecb601f8d378d03e83f895eb8134614ee9
Natraj16 <154439734+Natraj16@users.noreply.github.com>
2025-04-28 18:19:30 +0000

    datatype

---
+++ basics/datatypes_sum.js
@@ -0,0 +1,31 @@
+const score=100;
+const scoreValue=100.3
+
+const isLoggedIn=false
+const outsideTemp=null
+
+let n;
+
+const id=Symbol("123")
+const anotherId=Symbol("123")
+
+console.log(id===anotherId);
+
+const bigNum=515464344354354354354n
+
+const heroes=["shaktiman","natraj","doga"]
+
+let myObj={
+    name:"raj",
+    roll:"117"
+}
+
+const myFunc=function(){
+    console.log("hello world");
+    
+}
+
+console.log(typeof(bigNum));
+console.log(typeof(myFunc));
+console.log(typeof(heroes))
+console.log(typeof myObj)
