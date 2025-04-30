8442cf962533a2ae34a965568df1f83cd6ffcfe8
Natraj16 <154439734+Natraj16@users.noreply.github.com>
2025-04-30 15:27:21 +0000

    arrays

--- basics2/01_arrays.js
+++ basics2/01_arrays.js
@@ -1,10 +1,38 @@
 //array
 
-const myArr=[1,2,3,4,5,6]
+ const myArr=[1,2,3,4,5,6]
 
-console.log(myArr[0]);
+// console.log(myArr[0]);
 
-const myHeroes =["iron man","spiderman"]
+// const myHeroes =["iron man","spiderman"]
 
-const arr=new Array(1,2,3);
-console.log(arr[1]);
+// const arr=new Array(1,2,3,4,5);
+// console.log(arr[1]);
+
+//Array methods
+
+// arr.push(25)//add value
+// arr.push(28)
+// arr.pop()//remove the last element
+
+// // arr.unshift(5)//add to first index 
+// arr.shift()//removes the first index element
+
+
+// console.log(myArr.includes(9));//checks whether element is there and returns true and false
+// console.log(myArr.indexOf(9));//gives index of the passed element
+
+// const newArr= myArr.join()//convert the array as string
+// console.log(myArr);
+// console.log(newArr);
+
+// console.log(arr);
+//slice splie
+
+
+
+const myn1= myArr.slice(1,3)//return slice of the array
+console.log(myn1);
+console.log("B ", myArr);
+
+
